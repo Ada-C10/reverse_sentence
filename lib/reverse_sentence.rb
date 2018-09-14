0994b36edafbc398d08f83c1a90a9d5bcc71b508
Aurea <fu.sakura@gmail.com>
2018-09-13 17:22:50 -0700

    implement and pass all tests

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,4 +1,31 @@
 # A method to reverse the words in a sentence, in place.
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+
+  unless my_sentence.nil?
+    words = my_sentence.split(/(\s)/)
+
+    new_sentence = reverse_array(words).join
+
+    my_sentence.length.times do |i|
+      my_sentence[i] = new_sentence[i]
+    end
+  end
+end
+
+
+
+def reverse_array(arr)
+
+  unless arr.nil?  || arr.length < 2
+    length = arr.length
+
+    (length/2).times do |i|
+      tmp = arr[length - i - 1]
+      arr[length - i - 1] = arr[i]
+      arr[i] = tmp
+    end
+  end
+
+  return arr
+
 end
