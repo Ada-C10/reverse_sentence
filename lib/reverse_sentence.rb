c9ce963c8a34a7e3a477605d44c9a968d0d7dbf1
Katricia Smith <smithkatricia@gmail.com>
2018-09-19 08:25:48 -0700

    Add code from reverse string as reference

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,4 +1,19 @@
 # A method to reverse the words in a sentence, in place.
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+
+  if my_string == nil || my_string.length <= 1
+    return my_string
+  else
+    i = 0
+    j = my_string.length - 1
+    while i < j
+      temp = my_string[i]
+      my_string[i] = my_string[j]
+      my_string[j]=temp
+      i += 1
+      j -= 1
+    end
+    return my_string
+  end
+
 end
