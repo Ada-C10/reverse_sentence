4592f0ac4a7dbf89e212635359f5d7404081d16a
Divya <divyamohanan@gmail.com>
2018-09-26 11:20:26 -0700

    Sentence reversed

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,4 +1,44 @@
-# A method to reverse the words in a sentence, in place.
+#A method to reverse the words in a sentence, in place.
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  #Reverses a string that is passed
+  def string_reverse(string, start_index, end_index)
+      index = 0
+      length = end_index - start_index
+      while length != 0 && index <= (length/2)
+        swapped_element = string[start_index + index]
+        string[start_index + index] = string[end_index - index]
+        string[end_index - index] = swapped_element
+        index += 1
+      end
+      return string
+  end
+
+  if my_sentence != nil && my_sentence.length >= 3
+    length = my_sentence.length
+
+    #Reverses the sentence entirely
+    my_sentence = string_reverse(my_sentence, 0, length - 1)
+
+    index = 0
+    start_index = 0
+
+    while index < length
+
+      while my_sentence[index] == " "
+        if index + 1 != " "
+          start_index = index + 1
+        end
+        index += 1
+      end
+
+      while my_sentence[index] != " " && index < length
+        end_index = index
+        index += 1
+      end
+
+      #Reverses each reverses word of the sentence seperately
+      string_reverse(my_sentence, start_index, end_index)
+
+    end
+  end
 end
