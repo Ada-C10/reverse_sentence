3d49de952e602caf1e8982a95bb1d57ea4f6285a
Hayden Williams <hwilliams.deva@gmail.com>
2018-09-27 14:19:31 -0700

    implement reverse_sentence method

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,4 +1,28 @@
 # A method to reverse the words in a sentence, in place.
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  if my_sentence == nil || my_sentence.empty?
+    return my_sentence
+  end
+
+  i = 0
+  j = 0
+  temp = []
+
+  until i == my_sentence.length
+    until my_sentence[j] == " " && my_sentence[j + 1] != " " || my_sentence[j] != " " && my_sentence[j + 1] == " " || j == my_sentence.length - 1
+      j += 1
+    end
+
+    temp << my_sentence[i..j]
+
+    j += 1
+    i = j
+  end
+
+  total_index = my_sentence.length
+
+  temp.each do |capture|
+    total_index = total_index - capture.length
+    my_sentence[total_index..(total_index + capture.length - 1)] = capture
+  end
 end
