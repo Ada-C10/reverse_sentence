d2a17aa131a590a2dc2b5401ff2194365cde189e
Goeun Park <goeuntothepark@gmail.com>
2018-10-07 21:39:54 -0700

    combine string reverse and reverse word to create reverse sentence

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,4 +1,43 @@
+require 'pry'
+
 # A method to reverse the words in a sentence, in place.
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return my_sentence if my_sentence == nil || my_sentence.length == 0
+
+  low = 0
+  high = my_sentence.length - 1
+  while low < high
+    temp = my_sentence[low]
+    my_sentence[low] = my_sentence[high]
+    my_sentence[high] = temp
+    low += 1
+    high -= 1
+
+  end
+
+  index_of_spaces = []
+  my_sentence.length.times do |i|
+    if my_sentence[i] == " "
+      index_of_spaces << i
+    end
+  end
+  index_of_spaces << my_sentence.length
+
+  low = 0
+  high = index_of_spaces[0]-1
+
+  index_of_spaces.length.times do |i|
+    while low < high
+      temp = my_sentence[low]
+      my_sentence[low] = my_sentence[high]
+      my_sentence[high] = temp
+      low += 1
+      high -= 1
+    end
+    if i == index_of_spaces.length-1
+      return my_sentence
+    end
+    low = index_of_spaces[i]+1
+    high = index_of_spaces[i+1]-1
+  end
 end
